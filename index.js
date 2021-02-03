23fd7c72157a33287708960e9daced56548a512f
Caspar Steinebach <casparsteinebach@gmail.com>
2021-02-03 17:24:02 +0100

    setting up server with basic pages and req res

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
+const { SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS } = require('constants');
 const http = require('http');
+const fs = require('fs');
+
+const homePage = fs.readFileSync('index.html');
+const overmijPagina = fs.readFileSync('over.html');
+const contactPagina = fs.readFileSync('contact.html');
+const notFoundPagina = fs.readFileSync('notfound.html');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url);
-    res.end('Hij werkt!!');
+
+    if (req.url === '/') {
+        console.log(req.url);
+        res.end(homePage);
+    } else if (req.url === '/contact') {
+        res.end(contactPagina);
+    } else if (req.url === '/over') {
+        res.end(overmijPagina);
+    } else {
+        res.writeHead(404);
+        res.end(notFoundPagina);
+    }
+
+
 
 });
 
