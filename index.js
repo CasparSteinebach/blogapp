a26c73a4fd536825d9cadd111fdd074db2160a25
Caspar Steinebach <casparsteinebach@gmail.com>
2021-02-05 20:41:55 +0100

    express instantiated

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
 const { SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS } = require('constants');
 const http = require('http');
 const fs = require('fs');
+const express = require('express');
 
 const homePage = fs.readFileSync('index.html');
 const overmijPagina = fs.readFileSync('over.html');
 const contactPagina = fs.readFileSync('contact.html');
 const notFoundPagina = fs.readFileSync('notfound.html');
 
-const server = http.createServer((req, res) => {
+const app = express();
 
-    if (req.url === '/') {
-        console.log(req.url);
-        res.end(homePage);
-    } else if (req.url === '/contact') {
-        res.end(contactPagina);
-    } else if (req.url === '/over') {
-        res.end(overmijPagina);
-    } else {
-        res.writeHead(404);
-        res.end(notFoundPagina);
-    }
-
-
-
-});
-
-server.listen(3001);
+app.listen(3000, () => {
+    console.log('app listening on port 3000');
+})
