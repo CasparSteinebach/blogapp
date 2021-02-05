0d85a74f5e7c2dda92e147b1bced5db8382ebe4c
Caspar Steinebach <casparsteinebach@gmail.com>
2021-02-05 21:14:27 +0100

    assets

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const { SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS } = require('constants');
-const http = require('http');
-const fs = require('fs');
 const express = require('express');
-
-const homePage = fs.readFileSync('index.html');
-const overmijPagina = fs.readFileSync('over.html');
-const contactPagina = fs.readFileSync('contact.html');
-const notFoundPagina = fs.readFileSync('notfound.html');
+const path = require('path');
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log('app listening on port 3000');
+app.use(express.static('public'));
+
+app.listen(3001, () => {
+    console.log('app listening on port 3001');
 })
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+app.get('/over', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'over.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'contact.html'));
+});
